dcfe6d5e9371e7bf8e8df650b1c0881100b48f9b
Binisor Maria <lavinia.binisor@gmail.com>
2023-06-14 08:50:31 +0300

    initial commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by ionut.binisor on 14.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
